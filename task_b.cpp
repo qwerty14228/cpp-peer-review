3c40fe64d89e564258a9cc3c5577601f44aace68
qwerty14228 <115782814+qwerty14228@users.noreply.github.com>
2023-10-22 19:34:01 +0300

    Update task_b.cpp

--- task_b.cpp
+++ task_b.cpp
@@ -13,20 +13,27 @@ struct DateTime {
     int second;
 };
 
-void CheckRange(int value, const std::string& field_name, int min_value, int max_value) {
-    if (value < min_value || value > max_value) {
+struct Range {
+    int min;
+    int max;
+
+    Range(int min, int max) : min(min), max(max) {}
+};
+
+void CheckRange(int value, const std::string& field_name, const Range& range) {
+    if (value < range.min || value > range.max) {
         throw std::domain_error(field_name + " is out of range");
     }
 }
 
 void CheckDateTimeValidity(const DateTime& dt) {
-    CheckRange(dt.year, "year", 1, 9999);
-    CheckRange(dt.month, "month", 1, 12);
+    CheckRange(dt.year, "year", Range{1, 9999});
+    CheckRange(dt.month, "month", Range{1, 12});
 
     const bool is_leap_year = (dt.year % 4 == 0) && !(dt.year % 100 == 0 && dt.year % 400 != 0);
     const std::array<int, 12> month_lengths = {31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    CheckRange(dt.day, "day", 1, month_lengths[dt.month - 1]);
-    CheckRange(dt.hour, "hour", 0, 23);
-    CheckRange(dt.minute, "minute", 0, 59);
-    CheckRange(dt.second, "second", 0, 59);
+    CheckRange(dt.day, "day", Range{1, month_lengths[dt.month - 1]});
+    CheckRange(dt.hour, "hour", Range{0, 23});
+    CheckRange(dt.minute, "minute", Range{0, 59});
+    CheckRange(dt.second, "second", Range{0, 59});
 }
