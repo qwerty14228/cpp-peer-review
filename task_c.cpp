92484cdac9c5a8ebec0d5f7999abb6be01a0a814
qwerty14228 <115782814+qwerty14228@users.noreply.github.com>
2023-10-22 18:11:45 +0300

    Update task_c.cpp

--- task_c.cpp
+++ task_c.cpp
@@ -12,21 +12,32 @@ struct City {
     std::vector<std::string> languages;
 };
 
-void ParseCitySubjson(std::vector<City>& cities, const std::vector<std::string>& city_json,
-                      const std::string& country_name, const std::string& country_iso_code,
-                      const std::string& country_phone_code, const std::string& country_time_zone,
+void ParseCitySubjson(std::vector<City>& cities,
+                      const std::vector<std::string>& city_json,
+                      const std::string& country_name,
+                      const std::string& country_iso_code,
+                      const std::string& country_phone_code,
+                      const std::string& country_time_zone,
                       const std::vector<std::string>& languages) {
     for (const auto& city_str : city_json) {
         size_t pos = 0;
         std::vector<std::string> city_data;
+
         while ((pos = city_str.find(',')) != std::string::npos) {
             city_data.push_back(city_str.substr(0, pos));
             city_str.erase(0, pos + 1);
         }
         city_data.push_back(city_str);
 
-        cities.push_back({city_data[0], city_data[1],
-                          country_phone_code + city_data[2], country_name, country_iso_code,
-                          country_time_zone, languages});
+        City new_city;
+        new_city.name = city_data[0];
+        new_city.iso_code = city_data[1];
+        new_city.phone_code = country_phone_code + city_data[2];
+        new_city.country_name = country_name;
+        new_city.country_iso_code = country_iso_code;
+        new_city.country_time_zone = country_time_zone;
+        new_city.languages = languages;
+
+        cities.push_back(new_city);
     }
 }
